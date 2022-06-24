16f85f79e8cc8cf0821c4463ec3f4d3f56b6aec3
Achmad Fathullah <amedixro@gmail.com>
2022-06-24 14:33:34 +0700

    Initial Commit

---
+++ Coding challenge swift/setterGetter/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  Coding challenge swift
+//
+//  Created by Achmad Fathullah on 23/06/22.
+//
+
+import Foundation
+
+public class Product {
+    public var name: String? = nil
+    private var _quantity: Int = 0
+    public var quantity: Int{
+        set {
+            if newValue < 1 {
+                _quantity = 1
+            }else{
+                _quantity = newValue
+            }
+            
+        }
+        get { return _quantity }
+    }
+    
+    public init(quantity: Int) {
+        self.quantity = quantity;
+    }
+}
+
+let prod = Product(quantity: 1)
+prod.quantity = 0
+print(prod.quantity) // should print 1
